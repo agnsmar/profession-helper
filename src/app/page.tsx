dcfd4d8e0ae3df10e4e46c40dbe46b3d1fd4ec48
Denni Wernersson <dw222fy@student.lnu.se>
2024-01-27 15:56:04 +0100

    Add discord sign-in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
 import { unstable_noStore as noStore } from "next/cache";
+import Link from "next/link";
+import { getServerAuthSession } from "~/server/auth";
 
 export default async function Home() {
   noStore();
+  const session = await getServerAuthSession();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div className="absolute right-2 top-2 flex flex-row items-center justify-center gap-4">
+        <p className="text-white">
+          {session && <span>{session.user?.name}</span>}
+        </p>
+        <Link
+          href={session ? "/api/auth/signout" : "/api/auth/signin"}
+          className="rounded-md bg-white/10 px-2 py-1 font-semibold no-underline transition hover:bg-white/20"
+        >
+          {session ? "Sign out" : "Sign in"}
+        </Link>
+      </div>
+
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
           <span className="text-[hsl(280,100%,70%)]">Profession</span> Helper
