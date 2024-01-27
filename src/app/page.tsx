e765d15bae9d59281fcadaf5e45ce69bf124e76a
Denni Wernersson <dw222fy@student.lnu.se>
2024-01-27 17:57:43 +0100

    Restructure header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,23 +8,25 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="absolute right-2 top-2 flex flex-row items-center justify-center gap-4">
-        <p className="text-white">
-          {session && <span>{session.user?.name}</span>}
-        </p>
-        <Link
-          href={session ? "/api/auth/signout" : "/api/auth/signin"}
-          className="rounded-md bg-white/10 px-2 py-1 font-semibold no-underline transition hover:bg-white/20"
-        >
-          {session ? "Sign out" : "Sign in"}
-        </Link>
-      </div>
-
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+      <header className="flex flex-row justify-between p-2">
+        <h1 className="text-3xl font-extrabold tracking-tight">
           <span className="text-[hsl(280,100%,70%)]">Profession</span> Helper
         </h1>
-      </div>
+
+        <div className="flex flex-row items-center justify-center gap-4">
+          <p className="text-white">
+            {session && <span>{session.user?.name}</span>}
+          </p>
+          <Link
+            href={session ? "/api/auth/signout" : "/api/auth/signin"}
+            className="rounded-md bg-white/10 px-2 py-1 font-semibold no-underline transition hover:bg-white/20"
+          >
+            <span className="text-nowrap">
+              {session ? "Sign out" : "Sign in"}
+            </span>
+          </Link>
+        </div>
+      </header>
     </main>
   );
 }
