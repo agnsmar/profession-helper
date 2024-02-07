4f25f495192b1d8ce12df3795c569a7005b0d167
Denni Wernersson <dw222fy@student.lnu.se>
2024-02-07 22:35:00 +0100

    Remove character-checking on page

--- src/app/character/[id]/page.tsx
+++ src/app/character/[id]/page.tsx
@@ -2,15 +2,12 @@ import { unstable_noStore as noStore } from "next/cache";
 
 import { Character } from "../../_components/character";
 import { getServerAuthSession } from "~/server/auth";
-import { api } from "~/trpc/server";
 
 export default async function Home({ params }: { params: { id: string } }) {
   noStore();
   const session = await getServerAuthSession();
-  const character = await api.character.get.query({ id: Number(params.id) });
 
   if (!session?.user) return null;
-  if (!character) return null;
 
   return <Character id={params.id} />;
 }
