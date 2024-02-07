523cb0888992b2aa60977726d8b22c9a90cc90c1
Denni Wernersson <dw222fy@student.lnu.se>
2024-02-07 22:35:16 +0100

    Split delete character to component

--- src/app/_components/character.tsx
+++ src/app/_components/character.tsx
@@ -5,8 +5,26 @@ import { useRouter } from "next/navigation";
 import { api } from "~/trpc/react";
 
 export const Character = (props: { id: string }) => {
-  const router = useRouter();
+  const character = api.character.get.useQuery({ id: Number(props.id) });
 
+  if (!character.data) return null;
+
+  return (
+    <div className="flex flex-col gap-4 p-2">
+      <h1 className="text-3xl font-extrabold tracking-tight underline">
+        {character.data?.name}
+      </h1>
+      <h2 className="text-2xl font-extrabold tracking-tight">Dragonflight</h2>
+
+      <div className="flex w-[800px] flex-col">
+        <DeleteCharacter id={props.id} />
+      </div>
+    </div>
+  );
+};
+
+const DeleteCharacter = (props: { id: string }) => {
+  const router = useRouter();
   const deleteCharacter = api.character.delete.useMutation({
     onSuccess: () => {
       router.prefetch("/");
@@ -14,26 +32,20 @@ export const Character = (props: { id: string }) => {
   });
 
   return (
-    <div className="flex flex-col gap-4 p-2">
-      <h1 className="text-3xl font-extrabold tracking-tight underline">
-        Character ID: {props.id}
-      </h1>
-      <h2 className="text-2xl font-extrabold tracking-tight">Dragonflight</h2>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          deleteCharacter.mutate({ id: Number(props.id) });
-        }}
-        className="flex flex-col gap-2"
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        deleteCharacter.mutate({ id: Number(props.id) });
+      }}
+      className="flex flex-col gap-2"
+    >
+      <button
+        type="submit"
+        className="w-min select-none text-nowrap rounded-md bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
+        disabled={deleteCharacter.isLoading}
       >
-        <button
-          type="submit"
-          className="select-none rounded-md bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
-          disabled={deleteCharacter.isLoading}
-        >
-          {deleteCharacter.isLoading ? "Deleting..." : "Delete Character"}
-        </button>
-      </form>
-    </div>
+        {deleteCharacter.isLoading ? "Deleting..." : "Delete Character"}
+      </button>
+    </form>
   );
 };
