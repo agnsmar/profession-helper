dcddb3205d06fd03045686f98951bbc471c70539
Denni Wernersson <dw222fy@student.lnu.se>
2024-02-12 12:42:37 +0100

    Bind professions to trpc router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { characterRouter } from "./routers/character";
+import { professionRouter } from "./routers/profession";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { characterRouter } from "./routers/character";
  */
 export const appRouter = createTRPCRouter({
   character: characterRouter,
+  profession: professionRouter,
 });
 
 // export type definition of API
